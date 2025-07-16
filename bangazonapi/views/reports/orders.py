498cfbd391370a1c017ee6ada1d6cc5d301adb7f
Luke Davids <lukedavids@gmail.com>
2025-07-16 15:10:27 -0500

    Incomplete Orders Report Implemented

--- bangazonapi/views/reports/orders.py
+++ bangazonapi/views/reports/orders.py
@@ -23,7 +23,22 @@ def orders_report(request):
         }
 
         return render(request, 'reports/paid_orders.html', context)
-    return render(request, 'reports/paid_orders.html', {'orders': []})
+    # return render(request, 'reports/paid_orders.html', {'orders': []})
 
-def incomplete_orders_report(request):
-    status = request.GET.get('status', '')
+    if status == 'incomplete':
+        unpaid_orders = Order.objects.filter(payment_type__isnull=True)
+
+        orders_data = []
+
+        for order in unpaid_orders:
+            total = sum([li.product.price for li in order.lineitems.all()])
+            orders_data.append({
+                'id': order.id,
+                'customer_name': f"{order.customer.user.first_name} {order.customer.user.last_name}",
+                'total': round(total, 2),
+            })
+        context = {
+            'orders': orders_data
+        }
+        
+        return render(request, 'reports/incomplete_orders.html', context)
